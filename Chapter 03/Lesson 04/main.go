d15c441277986cee212bd4ac0c1f876397d1cb41
Mike Darling <mike.darling1@gmail.com>
2025-06-27 21:33:54 -0400

    Lesson 03.04 finished.

---
+++ Chapter 03/Lesson 04/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(rw, req)
+	})
+}
+
+func (cfg *apiConfig) middlewareMetricsReader() http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		counter := cfg.fileserverHits.Load()
+		rw.Header().Add("Content-Type", "text/html")
+
+		rw.WriteHeader(200)
+		fmt.Fprintf(rw,
+			`<html>
+	<body>
+		<h1>Welcome, Chirpy Admin</h1>
+		<p>Chirpy has been visited %d times!</p>
+	</body>
+</html>`, counter)
+	})
+}
+
+func (cfg *apiConfig) middlewareMetricsReset() http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		cfg.fileserverHits.Store(0)
+		rw.WriteHeader(200)
+	})
+}
+
+func main() {
+	mux := http.NewServeMux()
+	var cfg apiConfig
+
+	mux.HandleFunc("/api/healthz", func(rw http.ResponseWriter, req *http.Request) {
+		rw.Header().Add("Content-Type", "text/plain; charset=utf-8")
+		rw.WriteHeader(200)
+		fmt.Fprint(rw, "OK")
+	})
+
+	mux.Handle("/admin/metrics", (&cfg).middlewareMetricsReader())
+
+	mux.Handle("/admin/reset", (&cfg).middlewareMetricsReset())
+
+	mux.Handle("/app/", (&cfg).middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
+
+	var server http.Server
+	server.Handler = mux
+	server.Addr = ":8080"
+
+	server.ListenAndServe()
+}
