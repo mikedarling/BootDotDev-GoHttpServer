56b30dec66f1af2d74730d84d5e52ef20f88a490
Mike Darling <mike.darling1@gmail.com>
2025-07-19 22:23:24 -0400

    Lesson 08.04 finished.

---
+++ Chapter 08/Lesson 04/sql/queries/users.sql
@@ -0,0 +1,29 @@
+-- name: CreateUser :one
+INSERT INTO users (id, created_at, updated_at, email, hashed_password)
+VALUES (
+    gen_random_uuid(), NOW(), NOW(), $1, $2
+)
+RETURNING *;
+
+-- name: UpdateUser :one
+UPDATE users
+SET email = $1,
+    hashed_password = $2,
+    updated_at = $3
+WHERE id = $4
+RETURNING *;
+
+-- name: DeleteUsers :exec
+DELETE
+FROM users;
+
+-- name: GetUserByEmail :one
+SELECT *
+FROM users
+WHERE email = $1;
+
+-- name: UpgradeUserById :one
+UPDATE users
+SET is_chirpy_red = TRUE
+WHERE id = $1
+RETURNING *;
