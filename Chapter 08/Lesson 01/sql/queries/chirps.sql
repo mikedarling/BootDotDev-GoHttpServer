f8d0a378c6cedc44217f5ee8d21cfd52992ba8e4
Mike Darling <mike.darling1@gmail.com>
2025-07-19 22:13:41 -0400

    Lesson 08.01 finished.

---
+++ Chapter 08/Lesson 01/sql/queries/chirps.sql
@@ -0,0 +1,26 @@
+-- name: CreateChirps :one
+INSERT INTO chirps (id, created_at, updated_at, body, user_id)
+VALUES (
+    gen_random_uuid(), NOW(), NOW(), $1, $2
+)
+RETURNING *;
+
+-- name: GetAllChirps :many
+SELECT *
+FROM chirps
+ORDER BY created_at;
+
+-- name: GetChirp :one
+SELECT *
+FROM chirps
+WHERE id = $1;
+
+-- name: GetChirpById :one
+SELECT *
+FROM chirps
+WHERE id = $1;
+
+-- name: DeleteChirpById :exec
+DELETE
+FROM chirps
+WHERE id = $1;
